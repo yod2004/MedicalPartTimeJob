f43c687e5e88480e306f0ba1fca973a69f1da2b3
yod2004 <yodai0903@gmail.com>
2025-11-14 12:27:44 +0900

    PID制御を実装したもののうまく行かなかった．しかしフィードフォワードでも割とうまいことはいく．大体の線形性はありそうだ．

--- ESC_and_HallSensor_poring/ESC_and_HallSensor_poring.ino
+++ ESC_and_HallSensor_poring/ESC_and_HallSensor_poring.ino
@@ -4,14 +4,20 @@ int pwmPin = 10;
 int StickYPin = A0;
 int StickXPin = A1;
 
-unsigned int frq = 100; // 周波数
+unsigned int frq = 500; // 周波数
 float duty = 0.5; // 指定したいデューティ比
+float dutyMax = 0.9;
+float dutyMin = 0.5;
+float RPMMax = 36600;
+
+float calcTime = 10.0;
 
 //for HallSensor
 int HallPin = 2;
 unsigned long pulseCount = 0;      // 割り込みを使っていないため volatile は不要
 unsigned long lastSpeedCalcTime = 0; // 最後に速度を計算した時刻
 float currentSpeedHz = 0;            // 計算された速度 (Hz)
+float targetRPM = 25000;
 
 // ホールセンサーの前の状態を記憶する変数 (RISINGエッジ検出用)
 int lastHallState = LOW;
@@ -38,8 +44,14 @@ float fmap(float x, float in_min, float in_max, float out_min, float out_max){
     return x_new;
 }
 
+float pid(float r, float y, float kp, float kp_div){
+  float e = r - y;
+  float u = (kp * e) / kp_div;
+  return u;
+}
+
 void setup() {
-  setupPWM(100);
+  setupPWM(500);
   Serial.begin(9600);
   pinMode(pwmPin, OUTPUT);
   
@@ -53,9 +65,9 @@ void setup() {
   
   lastSpeedCalcTime = millis(); // 速度計算の基準時刻を初期化
 
-  setDuty(0.3);
-  delay(2000);
-  setDuty(0.1);
+  setDuty(dutyMax);
+  delay(4000);
+  setDuty(dutyMin);
   delay(2000);
 }
 
@@ -71,13 +83,14 @@ void loop() {
 
 
   // --- 1. モーター制御 (Duty比の設定) ---
-  float duty = fmap(analogRead(StickYPin), 0.0, 1023.0, 0.1, 0.3);
+  float duty = fmap(analogRead(StickYPin), 0.0, 1023.0, dutyMin, dutyMax);//本来意味はないが，この文があるとRPMの精度が向上する．
+  duty = constrain(dutyMin + (dutyMax - dutyMin) * targetRPM / RPMMax, dutyMin, dutyMax);
   setDuty(duty);
   
   
   // --- 2. 速度計算 (1秒ごとに行う) ---
   unsigned long currentTime = millis();
-  if (currentTime - lastSpeedCalcTime >= 1000) { // 1000ms (1秒) 経過したら
+  if (currentTime - lastSpeedCalcTime >= calcTime) { // 1000ms (1秒) 経過したら
     
     // (割り込みを使っていないため、noInterrupts/interrupts は不要)
     unsigned long pulses = pulseCount; // カウント数をコピー
@@ -92,12 +105,15 @@ void loop() {
     lastSpeedCalcTime = currentTime; // 最後の計算時刻を更新
     
     
-    // --- 3. 結果をシリアルに出力 ---
-    Serial.print("Duty: ");
-    Serial.print(duty);
-    Serial.print("  |  Speed (RPM): ");
-    Serial.print(currentSpeedHz * 60);
-    Serial.print("  |  Speed (Hz): ");
-    Serial.println(currentSpeedHz);
+    // // --- 3. 結果をシリアルに出力 ---
+    // Serial.print("Duty: ");
+    // Serial.print(duty);
+    // Serial.print("  |  Speed (RPM): ");
+    // Serial.print(currentSpeedHz * 60);
+    // Serial.print("  |  Speed (Hz): ");
+    // Serial.println(currentSpeedHz);
+
+    // duty = constrain(dutyMin + pid(targetRPM, currentSpeedHz * 60, 1000, 10000000), dutyMin, dutyMax);
+    // setDuty(duty);
   }
 }
